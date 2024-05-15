31e3536590bf2e746240d76068a2df64d4bb7738
Nabwinsaud <nabinsaud2059@gmail.com>
2024-05-15 11:38:52 +0545

    Revert "added: auth"
    This reverts commit 431809a01490c770a61c2f52cdadb09fe73d7764.

--- fakeHashPassword.ts
+++
@@ -1,9 +0,0 @@
-export class FakeHashPassword {
-  hash(password: string) {
-    return password;
-  }
-
-  compare(password: string, hashedPassword: string) {
-    return password === hashedPassword;
-  }
-}
